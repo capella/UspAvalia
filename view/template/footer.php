e25c723510427b2ce35396f65282ab894141bd0a
capellaresumo <capellaresumo@gmail.com>
2017-03-06 21:49:29 -0300

    Adicionando sistema para identificacao de busca

--- view/template/footer.php
+++ view/template/footer.php
@@ -1,4 +1,16 @@
-	<script src="http://<?= $url; ?>/assets/js/bootstrap.min.js"></script>
+    <script src="http://<?= $url; ?>/assets/js/bootstrap.min.js"></script>
+    <script type="application/ld+json">
+        {
+          "@context": "http://schema.org",
+          "@type": "WebSite",
+          "url": "http://uspavalia.com/",
+          "potentialAction": {
+            "@type": "SearchAction",
+            "target": "http://uspavalia.com/?p=pesquisa&pesquisa={search_term_string}",
+            "query-input": "required name=search_term_string"
+          }
+        }
+    </script>
     </div><!-- /.container -->
     <title>USP Avalia</title>
   </body>
