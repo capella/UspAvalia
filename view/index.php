fb4be15d326551b6a1146413b759c551fe897c1b
capellaresumo <capellaresumo@gmail.com>
2017-03-06 21:50:17 -0300

    1488847817

--- view/index.php
+++ view/index.php
@@ -123,3 +123,17 @@ function pessoas(){
 
                      </div><!-- /.row -->
 
+        
+    <script type="application/ld+json">
+        {
+          "@context": "http://schema.org",
+          "@type": "WebSite",
+          "url": "http://uspavalia.com/",
+          "potentialAction": {
+            "@type": "SearchAction",
+            "target": "http://uspavalia.com/?p=pesquisa&pesquisa={search_term_string}",
+            "query-input": "required name=search_term_string"
+          }
+        }
+    </script>
+
