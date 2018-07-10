c240f5787ebcc4b5ebb80dfa872761b92fee99d9
capellaresumo <capellaresumo@gmail.com>
2018-07-10 14:36:45 +0200

    Add favicon

--- view/template/header.php
+++ view/template/header.php
@@ -6,6 +6,24 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="description" content="">
       <meta name="author" content="">
+      <!-- Favicons -->
+      <link rel="apple-touch-icon" sizes="57x57" href="<?= $url_full; ?>/assets/favicons/apple-icon-57x57.png">
+      <link rel="apple-touch-icon" sizes="60x60" href="<?= $url_full; ?>/assets/favicons/apple-icon-60x60.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="<?= $url_full; ?>/assets/favicons/apple-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="<?= $url_full; ?>/assets/favicons/apple-icon-76x76.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="<?= $url_full; ?>/assets/favicons/apple-icon-114x114.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="<?= $url_full; ?>/assets/favicons/apple-icon-120x120.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="<?= $url_full; ?>/assets/favicons/apple-icon-144x144.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="<?= $url_full; ?>/assets/favicons/apple-icon-152x152.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="<?= $url_full; ?>/assets/favicons/apple-icon-180x180.png">
+      <link rel="icon" type="image/png" sizes="192x192"  href="<?= $url_full; ?>/assets/favicons/android-icon-192x192.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="<?= $url_full; ?>/assets/favicons/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="96x96" href="<?= $url_full; ?>/assets/favicons/favicon-96x96.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="<?= $url_full; ?>/assets/favicons/favicon-16x16.png">
+      <link rel="manifest" href="<?= $url_full; ?>/assets/favicons/manifest.json">
+      <meta name="msapplication-TileColor" content="#101010">
+      <meta name="msapplication-TileImage" content="<?= $url_full; ?>/assets/favicons/ms-icon-144x144.png">
+      <meta name="theme-color" content="#101010">
       <!-- Bootstrap core CSS -->
       <link rel="stylesheet" type="text/css" href="<?= $url_full; ?>/assets/css/meu.css">
       <!-- Custom styles for this template -->
