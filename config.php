b10f377c4d2243f0e78085878eac749e587dbed8
Gabriel Capella <gabriel@capella.pro>
2019-06-10 23:50:59 -0300

    Add PHPMailer

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+/* configiracoes do banco de dados */
+$hostname = $_ENV["HOSTNAME"];
+$database = $_ENV["MYSQL_DATABASE"];
+$username = $_ENV["MYSQL_USER"];
+$password = $_ENV["MYSQL_PASSWORD"];
+
+/* chave de seguranca, usada para hash */
+$secret_key = $_ENV["SECRET_KEY"];
+
+/* tipo do hash */
+/* http://php.net/manual/en/function.hash.php */
+$hash = "sha256";
+
+/* chaves facebook */
+$appId_facebook = $_ENV["APPID_FACEBOOK"];
+$secret_facebook = $_ENV["SECRET_FACEBOOK"];
+
+/* sitename */
+$url = "uspavalia.com";
+$url_full = "https://uspavalia.com";
+
+
+/* smtp mail */
+$smtp_host = $_ENV["SMTP_HOST"];
+$smtp_username = $_ENV["SMTP_USERNAME"];
+$smtp_password = $_ENV["SMTP_PASSWORD"];
+$smtp_password = $_ENV["SMTP_PASSWORD"];
+
+?>
--- view/email.php
+++ view/email.php
@@ -1,5 +1,7 @@
 <title>Contato</title>
 <?php
+require __DIR__ . '/../config.php';
+use PHPMailer\PHPMailer\PHPMailer;
 
 if(isset($_POST['email']) && isset($_SESSION["k1"]) &&
      isset($_SESSION["k1"]) && $_SESSION["k1"] + $_SESSION["k2"] == $_POST['soma']) {
@@ -53,28 +55,29 @@ if(isset($_POST['email']) && isset($_SESSION["k1"]) &&
     died($error_message);
   }
 
-  $email_message = "Form details below.\n\n";
+  $email_message = "Form details below:\n\n";
 
-  function clean_string($string) {
-    $bad = array("content-type","bcc:","to:","cc:","href");
-    return str_replace($bad,"",$string);
-  }
-
-  $email_message .= "First Name: ".clean_string($first_name)."\n";
-  $email_message .= "Last Name: ".clean_string($last_name)."\n";
-  $email_message .= "Email: ".clean_string($email_from)."\n";
-  $email_message .= "Telephone: ".clean_string($telephone)."\n";
-  $email_message .= "Comments: ".clean_string($comments)."\n";
-
+  $email_message .= "First Name: ".$first_name."\n";
+  $email_message .= "Last Name: ".$last_name."\n";
+  $email_message .= "Email: ".$email_from."\n";
+  $email_message .= "Comments: ".$comments."\n";
      
+  // Settings
+  $mail = new PHPMailer();
+  $mail->IsSMTP();
+  $mail->CharSet = 'UTF-8';
 
-     
+  $mail->Host       = $smtp_host;
+  $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+  $mail->SMTPAuth   = true;                  // enable SMTP authentication
+  $mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+  $mail->Username   = $smtp_username; // SMTP account username example
+  $mail->Password   = $smtp_password;        // SMTP account password example
 
-  // create email headers
-  $headers = 'From: '.$email_from."\r\n".
-  'Reply-To: '.$email_from."\r\n" .
-  'X-Mailer: PHP/' . phpversion();
-  mail($email_to, $email_subject, $email_message, $headers);  
+  // Content
+  $mail->isHTML(true);                                  // Set email format to HTML
+  $mail->Subject = 'USPAVALIA - Contato';
+  $mail->Body    = $email_message;
 
 ?>
 
@@ -89,107 +92,59 @@ if(isset($_POST['email']) && isset($_SESSION["k1"]) &&
 $_SESSION["k1"] = rand(0,20);
 $_SESSION["k2"] = rand(0,20);
 
-
 ?>
 <form name="contactform" method="post" action="/email">
-
-<h3>Contato</h3><hr>
-<p>
-Aqui você pode solicitar  que sua avaliação ou  nome seja retirado. Este  também é o espaço para dar sugestões ou expressar sua opinião.
-</p><br />
-<table style="width:100%">
-
-<tr>
-
-<td width="33%" valign="top">
-
-<label for="first_name">Nome *</label>
-
-</td>
-
-<td width="67%" valign="top">
-
-<input  type="text" name="first_name" maxlength="50" size="30" class="form-control"><br>
-
-</td>
-
-</tr>
-
-<tr>
-
-<td valign="top">
-
-<label for="last_name">Sobrenome *</label>
-
-</td>
-
-<td valign="top">
-
-<input  type="text" name="last_name" maxlength="50" size="30" class="form-control"><br>
-
-</td>
-
-</tr>
-
-<tr>
-
-<td valign="top">
-
-<label for="email">Endereço de Email*</label>
-
-</td>
-
-<td valign="top">
-
-   <input  type="text" name="email" maxlength="80" size="30" class="form-control"><br>
-
-</td>
-
-</tr> 
-
-<tr>
-
-<td width="33%" valign="top">
-
-<label for="first_name">Quanto é <?= $_SESSION["k1"] ?> + <?= $_SESSION["k2"] ?>?</label>
-
-</td>
-
-<td width="67%" valign="top">
-
-<input  type="number" name="soma" maxlength="50" size="30" class="form-control"><br>
-
-</td>
-
-</tr>
-
-<tr>
-
-<td valign="top">
-  
-  <label for="comments">Mensagem *</label></td>
-
-<td valign="top">
-  
-  <textarea  name="comments" maxlength="1000" cols="25" rows="6" class="form-control btn-lg"></textarea>
-  
-  </td>
-
-</tr>
-
-<tr>
-
-<td colspan="2" style="text-align:center">
-
-<br>
-<input type="submit" class="btn btn-default" value="Enviar">  
-
-</td>
-
-</tr>
-
-</table>
-
+  <h3>Contato</h3>
+  <hr>
+  <p> Aqui você pode solicitar que sua avaliação ou nome seja retirado. Este também é o espaço para dar sugestões ou expressar sua opinião. </p>
+  <br />
+  <table style="width:100%">
+    <tr>
+      <td width="33%" valign="top">
+        <label for="first_name">Nome *</label>
+      </td>
+      <td width="67%" valign="top">
+        <input type="text" name="first_name" maxlength="50" size="30" class="form-control">
+        <br> </td>
+    </tr>
+    <tr>
+      <td valign="top">
+        <label for="last_name">Sobrenome *</label>
+      </td>
+      <td valign="top">
+        <input type="text" name="last_name" maxlength="50" size="30" class="form-control">
+        <br> </td>
+    </tr>
+    <tr>
+      <td valign="top">
+        <label for="email">Endereço de Email*</label>
+      </td>
+      <td valign="top">
+        <input type="text" name="email" maxlength="80" size="30" class="form-control">
+        <br> </td>
+    </tr>
+    <tr>
+      <td width="33%" valign="top">
+        <label for="first_name">Quanto é <?= $_SESSION["k1"] ?> + <?= $_SESSION["k2"] ?>?</label>
+      </td>
+      <td width="67%" valign="top">
+        <input type="number" name="soma" maxlength="50" size="30" class="form-control">
+        <br> </td>
+    </tr>
+    <tr>
+      <td valign="top">
+        <label for="comments">Mensagem *</label>
+      </td>
+      <td valign="top">
+        <textarea name="comments" maxlength="1000" cols="25" rows="6" class="form-control btn-lg"></textarea>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="2" style="text-align:center">
+        <br>
+        <input type="submit" class="btn btn-default" value="Enviar"> </td>
+    </tr>
+  </table>
 </form>
 <?
 }
